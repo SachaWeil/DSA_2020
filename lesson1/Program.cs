4a9030291f0152cb2bfd659bfaea6f8a7e414349
SachaWeil <sacha.weil@edu.devinci.fr>
2020-10-12 19:19:33 +0300

    sp

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -6,7 +6,7 @@ namespace lesson_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
